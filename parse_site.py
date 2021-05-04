6b3af87b86b7b9261f444fc8ef38a6c90fc3de39
Roman Kagan <don.eric@gmail.com>
2021-05-04 17:35:38 +0300

    add args

--- parse_site.py
+++ parse_site.py
@@ -1,30 +1,35 @@
+import argparse
+
+
 from vfat_crauler import VfatCrauler
-
-from constants import ETH_NAME
-from constants import ETH_ADDRESS
-
-from constants import BSC_NAME
-from constants import BSC_ADDRESS
+from constants import ETH_NAME, ETH_ADDRESS, BSC_NAME, BSC_ADDRESS, HECO_NAME, HECO_ADDRESS
 
 
-from constants import HECO_NAME
-from constants import HECO_ADDRESS
+ETH_RUN_LIST = None
+BSC_RUN_LIST = None
+HECO_RUN_LIST = None
 
-ETH_RUN_LIST = ['https://vfat.tools/alphadex/', 'https://vfat.tools/badger/', 'https://vfat.tools/bella/', 'https://vfat.tools/frax/', 'https://vfat.tools/harvest-finance/', 'https://vfat.tools/ichi/', 'https://vfat.tools/powerindex/', 'https://vfat.tools/ruler/', 'https://vfat.tools/siren/', 'https://vfat.tools/stabilize/', 'https://vfat.tools/truefi/', 'https://vfat.tools/usf/']
+def main(blockchain):
+   if blockchain in ('eth', 'all'):
+      vfat_crauler = VfatCrauler(ETH_ADDRESS, ETH_NAME, ETH_RUN_LIST)
+      vfat_crauler.run()
 
-BSC_RUN_LIST = ['https://vfat.tools/bsc/autofarm', 'https://vfat.tools/bsc/bake', 'https://vfat.tools/bsc/soup', 'https://vfat.tools/bsc/kimochi', 'https://vfat.tools/bsc/beluga/', 'https://vfat.tools/bsc/midasdollar', 'https://vfat.tools/bsc/mythic', 'https://vfat.tools/bsc/wault', 'https://vfat.tools/bsc/wantanmee', 'https://vfat.tools/bsc/beefy', 'https://vfat.tools/bsc/duckmoney', 'https://vfat.tools/bsc/deflate', 'https://vfat.tools/bsc/squirrel', 'https://vfat.tools/bsc/uranium', 'https://vfat.tools/bsc/bitblocks', 'https://vfat.tools/bsc/yolodraw']
+   if blockchain in ('bsc', 'all'):
+      bsc_crauler = VfatCrauler(BSC_ADDRESS, BSC_NAME, BSC_RUN_LIST)
+      bsc_crauler.run()
 
-
-def main():
-    vfat_crauler = VfatCrauler(ETH_ADDRESS, ETH_NAME, ETH_RUN_LIST)
-    vfat_crauler.run()
-
-    bsc_crauler = VfatCrauler(BSC_ADDRESS, BSC_NAME, BSC_RUN_LIST)
-    bsc_crauler.run()
-
-    heco_crauler = VfatCrauler(HECO_ADDRESS, HECO_NAME)
-    heco_crauler.run()
+   if blockchain in ('heco', 'all'):
+      heco_crauler = VfatCrauler(HECO_ADDRESS, HECO_NAME)
+      heco_crauler.run()
 
 
 if __name__ == '__main__':
-    main()
+   my_parser = argparse.ArgumentParser(description='Parse yields')
+
+   # Add the arguments
+   my_parser.add_argument('-b', '--blockchain', action='store', choices=['eth', 'bsc', 'heco', 'all'], default='heco')
+
+   # Execute the parse_args() method
+   args = my_parser.parse_args()
+   
+   main(args.blockchain)
---
+++ run_all.sh
@@ -0,0 +1,5 @@
+python parse_site.py -b bsc &
+
+python parse_site.py -b eth &
+
+python parse_site.py -b heco &
